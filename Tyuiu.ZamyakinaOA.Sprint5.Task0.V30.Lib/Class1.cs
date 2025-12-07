cb5b6228c0ac98d6c5666f402d33791f6be97952
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-12-07 21:21:27 +0500

    task0

--- Tyuiu.ZamyakinaOA.Sprint5.Task0.V30.Lib/Class1.cs
+++ Tyuiu.ZamyakinaOA.Sprint5.Task0.V30.Lib/Class1.cs
@@ -1,4 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
 
 using tyuiu.cources.programming.interfaces.Sprint5;
 
