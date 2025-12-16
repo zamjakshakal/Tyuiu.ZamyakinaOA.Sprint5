5cf6aecb068ede226a338a66ae3654844c5ee967
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-12-17 04:31:01 +0500

    task6

---
+++ Tyuiu.ZamyakinaOA.Sprint5.Task6.V13.Lib/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZamyakinaOA.Sprint5.Task6.V13.Lib
+{
+    public class DataService : ISprint5Task6V13
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int cnt = 0;
+            string str = File.ReadAllText(path);
+            str = str.ToUpper().Replace("СС", "*");
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '*')
+                {
+                    cnt += 1;
+                }
+            }
+            return cnt;
+        }
+    }
+}
